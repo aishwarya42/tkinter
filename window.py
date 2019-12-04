a2ddf0b0a7336b131ffe51f603854d65c971e18d
aishwarya42 <42571918+aishwarya42@users.noreply.github.com>
2019-12-04 19:11:56 +0530

    Create window.py

---
+++ window.py
@@ -0,0 +1,11 @@
+import tkinter
+
+class Window(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.title("Window")
+        self.geometry("400x300")
+
+if __name__ == "__main__":
+    application = Window()
+    application.mainloop()
